2895656adf5764c2766e232ab021aa929ac9c2be
Leroy <llinstad20@gmail.com>
2022-05-05 17:53:41 -0500

    Leroy

--- ProjectNinja/code/enemyBase.gd
+++ ProjectNinja/code/enemyBase.gd
@@ -6,8 +6,11 @@ var movementDirection = Vector2(-1, 0)
 export var HP = 0
 export var damageToPlayer = 5
 export var speed = 10
+const GRAVITY = 15
+const JUMP_HEIGHT = -400
+const MAXFALLSPEED = 200
 
-var yVelo = 0
+var y_velo = 0
 const max_fall_speed = 40
 
 func _physics_process(delta):
@@ -16,6 +19,13 @@ func _physics_process(delta):
 	
 	direction = direction + movementDirection
 	
+	y_velo += GRAVITY
+	
+	direction.y = y_velo
+	
+	if y_velo > MAXFALLSPEED:
+		y_velo = MAXFALLSPEED
+	
 	move_and_slide(direction * speed)
 	
 	
@@ -23,7 +33,7 @@ func flip():
 	
 	movementDirection *= -1
 	
-	
+
 
 	
 	
